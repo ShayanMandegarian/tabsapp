2b43701c5ec6c4bece71a55be6803c0a9c8e6d2e
Shayan Mandegarian <smandegarian@yahoo.com>
2018-08-15 09:52:51 -0700

    8/14 update

--- www/js/app.js
+++ www/js/app.js
@@ -35,6 +35,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
@@ -63,15 +69,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
+  .state('tab.chat-detail', {
+    url: '/chats/:chatId',
+    views: {
+      'tab-chats': {
+        templateUrl: 'templates/chat-detail.html',
+        controller: 'ChatDetailCtrl'
       }
-    })
+    }
+  })
 
   .state('tab.account', {
     url: '/account',
@@ -84,6 +90,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/login');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,6 +17,169 @@ angular.module('starter.controllers', [])
   };
 })
 
+.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+  $scope.data = {};
+  $scope.login = function() {
+      LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+          $state.go('tab.dash');
+      }).error(function(data) {
+          var alertPopup = $ionicPopup.alert({
+            title: 'Login failed!',
+            template: '<center>Please check your credentials!</center>',
+            buttons: [
+              {
+              type: 'button-royal',
+              text: "OK"
+              }
+            ]
+          });
+      });
+  }
+})
+
+.controller('HttpCtrl', function($scope, $rootScope, $http, $ionicPopup) {
+  $scope.start = 0;
+  $scope.end = 10;
+  $rootScope.page = ($scope.start / 10) + 1;
+  $http({
+    method: 'GET', url: 'http://localhost/pt_admin/server/index.php/ionic',
+    params: {start: $scope.start, end: $scope.end}
+  }).then(function(response) {
+    $scope.content = response.data.rows;
+    $rootScope.total = Math.floor(response.data.total.count);
+    $rootScope.pages = Math.floor($rootScope.total / 10);
+  });
+
+  $scope.nextparams = function(start, end) {
+    $scope.start = Math.floor($scope.start + 10);
+    if ($scope.start > ($rootScope.total - 10)) {
+      $scope.start = Math.floor($rootScope.total - 10);
+    }
+    $scope.end = $scope.end + 10;
+    $rootScope.page = Math.floor(($scope.start / 10)) + 1;
+    $http({
+      method: 'GET', url: 'http://localhost/pt_admin/server/index.php/ionic',
+      params: {start: $scope.start, end: $scope.end}
+    }).then(function(response) {
+      $scope.content = response.data.rows;
+    });
+
+    return;
+  };
+
+  $scope.lastparams = function(start, end) {
+    $scope.start = Math.floor($scope.start - 10);
+    $scope.end = $scope.end - 10;
+    if($scope.start < 0) {
+      $scope.start = 0;
+    }
+    if($scope.end < 10) {
+      $scope.end = 10;
+    }
+    $rootScope.page = Math.floor(($scope.start / 10)) + 1;
+    $http({
+      method: 'GET', url: 'http://localhost/pt_admin/server/index.php/ionic',
+      params: {start: $scope.start, end: $scope.end}
+    }).then(function(response) {
+      $scope.content = response.data.rows;
+    });
+
+    return;
+  };
+
+  $scope.finalparams = function() {
+    $scope.start = Math.floor($rootScope.total - 10);
+    $rootScope.page = Math.floor($rootScope.pages);
+    $http({
+      method: 'GET', url: 'http://localhost/pt_admin/server/index.php/ionic',
+      params: {start: $scope.start, end: $scope.end}
+    }).then(function(response) {
+      $scope.content = response.data.rows;
+    });
+  };
+
+  $scope.firstparams = function() {
+    $scope.start = 0;
+    $rootScope.page = ($scope.start / 10) + 1;
+    $http({
+      method: 'GET', url: 'http://localhost/pt_admin/server/index.php/ionic',
+      params: {start: $scope.start, end: $scope.end}
+    }).then(function(response) {
+      $scope.content = response.data.rows;
+    });
+  };
+
+  $scope.addEntry = function(ent) {
+    if(!ent.user || !ent.pass) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Error!',
+        template: '<center>You must enter a username and password</center>',
+        buttons: [
+          {
+          type: 'button-royal',
+          text: "OK"
+          }
+        ]
+      });
+      ent.user='';
+      ent.pass='';
+      return;
+    }
+    
+    if(ent.user.length < 3 || ent.pass.length > 45) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Error!',
+        template: '<center>Your username must be between 3 and 45 chars long</center>',
+        buttons: [
+          {
+          type: 'button-royal',
+          text: "OK"
+          }
+        ]
+      });
+      ent.user='';
+      ent.pass='';
+      return;
+    }
+
+    if(ent.pass.length < 5 || ent.pass.length > 45) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Error!',
+        template: '<center>Your password must be between 5 and 45 chars long</center>',
+        buttons: [
+          {
+          type: 'button-royal',
+          text: "OK"
+          }
+        ]
+      });
+      ent.user='';
+      ent.pass='';
+      return;
+    }
+
+    $http({
+      method: 'POST', url: 'http://localhost/pt_admin/server/index.php/ionic',
+      params: {user: ent.user, pass: ent.pass}
+    }).then(function(response) {
+      $rootScope.addContent = response.data.msg;
+    });
+    var alertPopup = $ionicPopup.alert({
+      title: 'New entry added!',
+      // template: $rootScope.addContent,
+      buttons: [
+        {
+        type: 'button-royal',
+        text: "OK"
+        }
+      ]
+    });
+    ent.user='';
+    ent.pass='';
+    return;
+  };
+})
+
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
